25a06cec4364a4fc657baac748100e965584fb8e
AlondraRosendo <91708278+AlondraRosendo@users.noreply.github.com>
2021-11-19 15:00:57 -0600

    Create FuncionAlgebraica3.m

---
+++ FuncionAlgebraica3.m
@@ -0,0 +1,16 @@
+%Octave Script
+%Title:  Funciones algebraicas: polinomiales y racionales
+%Descripcion:  Script para graficar una funcion
+%Autora:  Alondra Cayetano Rosendo
+%Escuela:  Tecnologico de Estudios Superiores de Jilotepec
+%Date:  19-11-2021
+%Funcion:  3
+
+clear
+pkg load symbolic
+syms x
+x=(0:0.1:10);
+fx= (x+2)./(x-1);
+plot(x,fx)
+ylabel('x')
+xlabel('fx')
